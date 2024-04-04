00986e299a34d7c73cf1b8939b454cf157158216
Aashish Anand <anandaashish512@gmail.com>
2024-04-04 22:30:21 +0530

    snsn

---
+++ pages/server/users.tsx
@@ -0,0 +1,11 @@
+const UserPage=()=>{
+    return (
+        <div>
+            <h1>Users (SSR)</h1>
+        </div>
+    )
+}
+export const getServerSideProps=async ()=>{
+
+};
+export default UserPage;
